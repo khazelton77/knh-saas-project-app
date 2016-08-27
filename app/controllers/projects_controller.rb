480c022f54cbe630152c3435581ea3b754d8ee35
khazelton77 <keri.hazelton@gmail.com>
2016-08-27 14:07:40 -0400

    Add artifact

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,3 +1,4 @@
+
 class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
   before_action :set_tenant, only: [:show, :edit, :update, :destroy, :new, :create]
