1b8eaf6fe55ad8423abec60506ffe8eb794db141
khazelton77 <keri.hazelton@gmail.com>
2016-08-29 10:28:02 -0400

    Get app ready for deployment

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,28 +4,28 @@ class Project < ActiveRecord::Base
   has_many :artifacts, dependent: :destroy
   has_many :user_projects
   has_many :users, through: :user_projects
-  validate :free_plan_can_only_have_one_project 
-
+  validate :free_plan_can_only_have_one_project
+  
   def free_plan_can_only_have_one_project
-  	if self.new_record? && (tenant.projects.count > 0) && (tenant.plan == 'free')
-  		errors.add(:base, "Free plans cannont have more than one project.")
-  	end
+    if self.new_record? && (tenant.projects.count > 0) && (tenant.plan == 'free')
+      errors.add(:base, "Free plans cannot have more than one project")
+    end
   end
-
-  def by_user_plan_and_tenant(tenant_id, user)
-  	tenant = Tenant.find(tenant_id)
-  	if tenant.plan == 'premium'
-  		if user.is_admin?
+  
+  def self.by_user_plan_and_tenant(tenant_id, user)
+    tenant = Tenant.find(tenant_id)
+    if tenant.plan == 'premium'
+      if user.is_admin?
         tenant.projects
-      else 
+      else
         user.projects.where(tenant_id: tenant.id)
       end
     else
       if user.is_admin?
-  		  tenant.projects.order(:id).limit(1)
+        tenant.projects.order(:id).limit(1)
       else
         user.projects.where(tenant_id: tenant.id).order(:id).limit(1)
       end
-  	end
+    end
   end
 end
